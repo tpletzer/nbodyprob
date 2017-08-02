105cfd66c40df3a25c7b56080f751c57675927c6
tpletzer <tamara.pletzer@gmail.com>
2017-08-02 11:22:34 +0100

    Update energy3.m

--- energy3.m
+++ energy3.m
@@ -19,7 +19,7 @@ function et = energy3(y, m1, m2, m3)
     r23 = sqrt((x3-x2).^2 + (y3-y2).^2);
 
     ek = (m1*(vx1.^2 + vy1.^2) + m2*(vx2.^2 + vy2.^2) + m3*(vx3.^2 + vy3.^2))/2;
-    %double check the -1/2
+  
     ep = -(((m1*m2)./r12) + ((m1*m3)./r13) + ((m2*m3)./r23));
 
     et = ek + ep;
